fbe88dba658338d4321b6e5006465bf34fd5fe3d
Wolfgangyuan <wolfgangyuan0@gmail.com>
2026-03-05 10:19:45 +0100

    add log part

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ function showContent(id) {
     });
     document.getElementById(id+'-button').classList.add('active');
 }
+
+const mdFile = '/Logs/2026.2.24.md';
+
+const mdContainer = document.getElementById('log-container');
+fetch(mdFile)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(md => {
+        const htmlContent = marked.parse(md);
+        mdContainer.innerHTML = htmlContent;
+    })
+    .catch(error => {
+        console.error('Error fetching markdown file:', error);
+        mdContainer.innerHTML = '<p>Error loading log content.</p>';
+    });
